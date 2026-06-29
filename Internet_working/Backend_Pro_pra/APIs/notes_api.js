9a37d57582e6b34b458db02f45cd267de14a8773
devraj-jha <devrajjha100806@gmail.com>
2026-06-29 20:24:36 +0530

    notes API COMPLETE

--- Internet_working/Backend_Pro_pra/APIs/notes_api.js
+++ Internet_working/Backend_Pro_pra/APIs/notes_api.js
@@ -1,9 +1,34 @@
 const express = require("express");
-
+let notes = [
+    {
+        id: 1,
+        text: "learn express"
+    }, 
+    {
+        id: 2,
+        text: "go to gym"
+    }
+]
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello Backend!");
+    res.send("Home");
+});
+
+app.get("/notes", (req, res) => {
+    res.send(["Math", "Physics"]);
+});
+
+app.post("/notes", (req, res) => {
+    res.send("Note Created");
+});
+
+app.delete("/notes", (req, res) => {
+    res.send("Deleted");
+});
+
+app.get("/notes/:id", (req, res) => {
+    res.send("Note " + req.params.id);
 });
 
 app.listen(3000);
